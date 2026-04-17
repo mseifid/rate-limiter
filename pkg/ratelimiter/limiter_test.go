fa361fabff24183f759f12ef226209f69d4a7ded
mseifid <mohammad.seifi.d@gmail.com>
2026-04-17 18:08:00 +0330

    test: added test for limiter

---
+++ pkg/ratelimiter/limiter_test.go
@@ -0,0 +1,35 @@
+package ratelimiter
+
+import (
+	"context"
+	"sync"
+	"sync/atomic"
+	"testing"
+)
+
+func TestLimiterAllow(t *testing.T) {
+	limiter := NewLimiter(NewInMemoryStore())
+	const userID string = "4"
+	ctx := context.WithValue(context.Background(), "userID", userID)
+	var wg sync.WaitGroup
+
+	counter := int64(0)
+
+	for range 1_000_000 {
+		wg.Go(func() {
+			res, err := limiter.Allow(ctx)
+			if err != nil {
+				t.Errorf("Allow function returned error")
+			}
+			if res.Allowed {
+				atomic.AddInt64(&counter, 1)
+			}
+		})
+	}
+
+	wg.Wait()
+
+	if counter < UserBucketCapacity {
+		t.Errorf("oh no! expected: %v, actual: %v", UserBucketCapacity, counter)
+	}
+}
